02a910c45195b1a22af77b7ee3edcd2430c52c8c
Aryan Saxena <58971587+aryans31@users.noreply.github.com>
2024-06-07 17:53:50 +0530

    adding application testing code

---
+++ transportSSRC-master/src/test/java/com/brodbill/transportssrc/TransportSsrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brodbill.transportssrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportSsrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
